9d94c9aa63db372658708a97556b103865cb1f2d
KCushUX <124751865+KCushUX@users.noreply.github.com>
2023-03-16 23:09:31 -0400

    Update scripts.js
    - Added JavaScript code to toggle the menu on and off.
    - Added JavaScript code for smooth scrolling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log("Thanks for coming!");
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    } else {
+        x.className = 'navtoggle';
+    }
+}
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 2000);
+});
